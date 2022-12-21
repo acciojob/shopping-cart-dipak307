8ef220123945cc83180eb6afdaae5a584ed0319f
dipak307 <114248721+dipak307@users.noreply.github.com>
2022-12-21 12:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your code here
+let itemInput=document.getElementById("item-name-input");
+let priceInput=document.getElementById("item-price-input");
+let button=document.getElementById("add");
+let table=document.getElementById("table");
+let total=document.getElementById("total");
+
+let priceArr=[];
+function addItem() {
+    let item=itemInput.value;
+    let price=priceInput.value;
+    let sum=0;
+    
+    if(item !=='' && price!==''){
+        let row=document.createElement('tr');
+        priceArr.push(parseInt(price));
+        row.innerHTML=`<td>${item} </td> <td>${price} </td> `
+        for(let i=0;i<priceArr.length;i++){
+            sum+=priceArr[i];
+            
+        }
+        table.append(row);
+        total.innerText=sum;
+        itemInput.value="";
+        priceInput.value="";
+    }
+}
+
+button.addEventListener('click',addItem);
